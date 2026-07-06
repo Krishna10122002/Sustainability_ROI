cdea92ce08b19924081b5591f544e8c796ac3b97
Krishna10122002 <159173909+Krishna10122002@users.noreply.github.com>
2026-07-07 04:19:05 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,7 @@
+NetBenefit = Savings + RevenueImpact + RiskAvoided - OperatingCost;
 
+ROI = (NetBenefit / Investment) * 100;
+
+Payback = Investment / NetBenefit;
+
+CarbonValue = CarbonReduction * CarbonPrice;
